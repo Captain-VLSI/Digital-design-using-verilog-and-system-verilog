a24f2aae955e4cbb24255c210971eb305e6ef23f
GANESH S <165929138+Captain-VLSI@users.noreply.github.com>
2025-01-08 19:25:34 +0530

    mux81tb.v

--- mux81tb.v
+++ mux81tb.v
@@ -1,22 +1,22 @@
 module mux81tb;
 wire y;
-reg [7:0]I;
+reg [7:0]i;
 reg [2:0]s;
-integer i;
+//integer i;
 
 mux81 dut (.*);
 
 initial begin
-$monitor($time,"y=%b,I=%b,s=%b",y,I,s);
-
-s<=0;
-I<=$random;
-
-for (i = 1;i<4;i=i+1)
-    begin
-#5 s<=i;
-    end
-
-#10 $finish();
-end
+$monitor($time,"i=%b,s=%b,y=%b",i,s,y);
+           i=8'b0000_0000;
+     #10   i=8'b0000_0001;s=3'b000;
+     #10   i=8'b0000_0010;s=3'b001;
+     #10   i=8'b0000_0100;s=3'b010;
+     #10   i=8'b0000_1000;s=3'b011;
+     #10   i=8'b0001_0000;s=3'b100;
+     #10   i=8'b0010_0000;s=3'b101;
+     #10   i=8'b0100_0000;s=3'b110;
+     #10   i=8'b1000_0000;s=3'b111;
+   #200 $finish();
+        end
 endmodule
