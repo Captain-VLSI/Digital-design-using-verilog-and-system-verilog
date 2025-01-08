5a0d398c27dc486e49732fe8d59de0485fede16a
GANESH S <165929138+Captain-VLSI@users.noreply.github.com>
2025-01-08 19:26:22 +0530

    mux81.v

--- mux81.v
+++ mux81.v
@@ -1,13 +1,19 @@
-/////////data flow ////////
-module mux81 (input [7:0]I,
+module mux81 (input [7:0]i,
               input [2:0]s,
-              output y);
-assign y =((~s[0]) & (~s[1]) & (~s[2]) & I[0]) | 
-           ((~s[0]) & (~s[1]) & (s[2]) & I[1]) | 
-           ((~s[0]) & (s[1]) & (~s[2]) & I[2]) | 
-           ((~s[0]) & (s[1]) & (s[2]) & I[3]) | 
-           ((s[0]) & (~s[1]) & (~s[2]) & I[4]) | 
-           ((s[0]) & (~s[1]) & (s[2]) & I[5]) | 
-           ((s[0]) & (s[1]) & (~s[2]) & I[6]) | 
-           ((s[0]) & (s[1]) & (s[2]) & I[7]);
-endmodule           
+              output reg y);
+
+always @(s,i)
+begin
+case (s)
+3'b000: y = i[0];
+3'b001: y = i[1];
+3'b010: y = i[2];
+3'b011: y = i[3];
+3'b100: y = i[4];
+3'b101: y = i[5];
+3'b110: y = i[6];
+3'b111: y = i[7];
+default:y=0;
+endcase
+end
+endmodule
